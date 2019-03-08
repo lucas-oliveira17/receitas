ac55d9a8526578b2e4ef2d59a1c2e745ae4ed710
Lucas de Oliveira Viana <lucas.viana@db1.com.br>
2019-03-08 17:27:41 -0300

    a

--- src/front.js
+++ src/front.js
@@ -1,16 +1,40 @@
-import { pesquisaPorIngrediente } from "./pesquisarReceitas"
+import {
+    pesquisaPorIngrediente
+} from "./pesquisarReceitas"
 
 let toStr = function (ingrediente) {
     let str = "";
+    let strIng = "";
     pesquisaPorIngrediente(ingrediente).forEach(receita => {
+        receita.ingredientes.forEach(ingrediente => {
+            strIng += "- " + ingrediente + "<br>"
+        })
         str += "Nome: " + receita.nome + "<br>" +
-               "Autor: " + receita.autor + "<br>" +
-               "Ingredientes: " + receita.ingredientes + "<br>" +
-               "Modo de Preparo: " + receita.modoDePreparo + "<br><br>"
+            "Autor: " + receita.autor + "<br>" +
+            "Ingredientes: " + strIng +
+            "Modo de Preparo: " + receita.modoDePreparo + "<br><br>"
     })
     return str;
 }
 
-document.getElementById("btnok").addEventListener("click", function () {
-    document.getElementById("result").innerHTML = toStr(document.getElementById("caixaIngrediente").value);
-})
+pressOk();
+
+pressAdd();
+
+function pressOk() {
+    document.getElementById("btnok").addEventListener("click", function () {
+        document.getElementById("ingrediente");
+        document.getElementById("result").innerHTML = toStr(document.getElementById("caixaIngrediente").value);
+    });
+}
+
+function pressAdd() {
+    let counter = 0;
+    document.getElementById("add").addEventListener("click", addInput());
+
+    function addInput() {
+        var cx = document.getElementById("caixaIngrediente");
+        document.getElementById("add2").innerHTML += cx;
+        counter++;
+    };
+}
