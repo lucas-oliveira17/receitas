5a11e2ef87cfaffce3e2a9c9910e8564ee30c4e0
Lucas de Oliveira Viana <lucas.viana@db1.com.br>
2019-03-12 09:35:58 -0300

    pesquisa dinamica funcionando c 1 ingrediente

--- src/front.js
+++ src/front.js
@@ -19,29 +19,43 @@ let toStr = function (ingrediente) {
 
 function pressOk() {
     document.getElementById("btnok").addEventListener("click", function () {
-        let ingrediente = document.getElementById("ingr").childNodes;
-        let array = Array.from(ingrediente)
-        array.forEach(campo => {
-            console.log(campo.value);
+        let ingrediente = pegarFilhos("ingrediente");
+        let arrayDeFilhos = passarFilhosParaArray(ingrediente)
+        arrayDeFilhos.forEach(filhoIngrediente => {
+            console.log(filhoIngrediente.value);
+            document.getElementById("result").innerHTML += toStr(filhoIngrediente.value);
         })
     });
+}
+
+function passarFilhosParaArray(ingrediente) {
+    return Array.from(ingrediente);
+}
+
+function pegarFilhos(elemento) {
+    return document.getElementById(elemento).childNodes;
 }
 
 function addInput() {
     let counter = 1;
     document.getElementById("btnadd").addEventListener("click", function () {
 
-        let ingrediente = document.getElementById("ingr");
+        let ingrediente = document.getElementById("ingrediente");
 
-        let text = "<input type='text' id='campoIngrediente" + counter + "' <style='margin:10px 0px'>";
+        let text = document.createElement("input");
+
+        text.innerHTML = "<input type='text' id='campoIngrediente" + counter + "'";
+        text.setAttribute("style", "margin: 8px 350px");
 
         ingrediente.appendChild(text);
 
+        console.log(ingrediente);
+
         document.getElementById("new").appendChild(ingrediente);
+
         counter++;
     })
 };
 
 pressOk();
-
 addInput();
