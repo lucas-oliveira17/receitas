babe279ce7994073636de13932b9c21e824681ee
Lucas de Oliveira Viana <lucas.viana@db1.com.br>
2019-03-18 14:26:46 -0300

    teste printaReceitas rodando

--- src/front.test.js
+++ src/front.test.js
@@ -1,5 +1,11 @@
 import  { printaReceita } from "./front"
 
 test('retorno do printaReceita',  () => {
-    expect(printaReceita(["1 Limão"])).toBe("");
+    expect(printaReceita(["1 Limão", "1L de Vodka"])).toBe(
+    `<ul>
+                <li>Nome: Caipirinha da Jessikill </li>
+                <li>Autor: Jessikill </li>
+                <li>Ingredientes:<ul> <li> 1 Limão </li><li> 1L de Vodka </li></ul></li>
+                <li>Modo de Preparo: Blabla </li>
+                </ul>`); 
 });
